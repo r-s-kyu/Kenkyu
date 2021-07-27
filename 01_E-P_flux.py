b98352bfeb0c96a31f79ba51d144a629f999fb94
Satake at Kyushu Univ. PC <satake.riku.263@s.kyushu-u.ac.jp>
2021-07-27 16:25:57 +0900

    improve

--- 01_E-P_flux.py
+++ 01_E-P_flux.py
@@ -9,13 +9,27 @@ a = 6.37e+6
 
 # %%
 import numpy as np
-file = f'C:/Users/riku satake/Documents/data/anl_p_hgt.2020.bin'
+fileHGT = f'C:/Users/riku satake/Documents/data/anl_p_hgt.2020.bin'
+fileT = f'C:/Users/riku satake/Documents/data/anl_p_tmp.2020.bin'
+fileU = f'C:/Users/riku satake/Documents/data/anl_p_ugrd.2020.bin'
+fileV = f'C:/Users/riku satake/Documents/data/anl_p_vgrd.2020.bin'
 
-f = open(file, 'rb')
 
-data = np.fromfile(f,dtype='>f').reshape(366,37,145,288)
-data = data[:,:,::-1]
-print(data[0,10])
+fHGT = open(fileHGT, 'rb')
+fT = open(fileT, 'rb')
+fU = open(fileU, 'rb')
+fV = open(fileV, 'rb')
+
+dataHGT = np.fromfile(fHGT,dtype='>f').reshape(366,37,145,288)
+dataT = np.fromfile(fT,dtype='>f').reshape(366,37,145,288)
+dataU = np.fromfile(fU,dtype='>f').reshape(366,37,145,288)
+dataV = np.fromfile(fV,dtype='>f').reshape(366,37,145,288)
+
+HGT4 = dataHGT[:,:,::-1]
+T4 = dataT[:,:,::-1]
+U4 = dataU[:,:,::-1]
+V4 = dataV[:,:,::-1]
+# print(dataT[0,10])
 
 
 # %%
@@ -34,7 +48,7 @@ Ts =  240
 phi = 
 f = 2*omega*math.sin(phi)
 
-T =250
+T = 250
 H = R*T/g0
 rhos = ps/R/Ts
 rho = rhos*math.e**((-1)*z/H)
