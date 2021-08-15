5b9847c44be588b4c4c22142ec3166f3ada9aeaf
Riku Satake <r.s.kyushu@gmail.com>
2021-08-15 18:34:55 +0900

    finish EP-flux_divergence

--- test.py
+++ test.py
@@ -5,8 +5,8 @@ import math
 from datetime import date
 
 year = 2020
-month = 9
-day = 20
+month = 2
+day = 1
 fday = date(year,1,1)
 
 dc = (date(year,month,day)-fday).days + 1
@@ -22,8 +22,12 @@ for kind in kindlist:
 
 pcord = np.array([1000,975,950,925,900,875,850,825,800,775,750,700,
         650,600,550,500,450,400,350,300,250,225,200,175,150,125,100,70,
-        50,30,20,10,7,5,3,2,1])*100
+        50,30,20,10,7,5,3,2,1])
 
+dp = np.array([])
+for i in range(len(pcord)-1):
+    dp = np.append(dp, pcord[i]-pcord[i+1])
+dp = np.append(dp,1)
 # print(pcord)
 
 phicord = np.arange(-90,91,1.25)*(math.pi/180.)
@@ -38,45 +42,33 @@ omega = 7.29e-5
 Ts =  240
 H = R*Ts/g0
 rhos = ps/R/Ts
-Fy = np.zeros((145,37),dtype=np.float64)*np.nan
-Fz = np.zeros((145,37),dtype=np.float64)*np.nan
-N_2 = 4.0e-4
 f = 2*omega*np.sin(phicord)
+rho = rhos*(pcord*100/ps)
 
 vudev_mean = np.mean(devvgrd*devugrd,axis=2)
-rho = rhos*(pcord/ps)
 Fy = (((-1)*a*vudev_mean*np.cos(phicord)).T*rho).T
+devFy = np.gradient(Fy*np.cos(phicord), phicord,axis=1)
 
+z = -H*np.log(pcord*100/ps)
 vTdev_mean = np.mean(devvgrd*devtemp,axis=2)
 Fz = ((a*np.cos(phicord)*f*R*vTdev_mean/(N_2*H)).T*rho).T
+devFz = np.gradient(Fz,z,axis=0)
 
-# theta = (datatmp.T*(ps/pcord)**K).T # 3次元
-# thetadev = (theta.T - np.mean(theta).T).T
-# vthetadev_mean = np.mean(devvgrd*thetadev,axis=2)
-# theta_z_dev = theta*K/H
-# Fz = ((a*np.cos(phicord)*f*vthetadev_mean/np.mean(theta_z_dev,axis=2)).T*rho).T
+nablaF = devFy/(a*np.cos(phicord)) + devFz
 
 vector_scale = 1.0e+6
 # vector_scale = 5.0e+4
 lim = 100
 mabiki = 5
-test1 = np.mean(Fy[-11:,:],axis=1)
-test2 = np.mean(Fz[-11:,:],axis=1)
-i = 5
-j = 20
-# print((Fy[-1*i,2::7]**2+(Fz[-1*i,2::7]*100)**2)**0.5/vector_scale)
-# print((Fy[-1*j,2::7]**2+(Fz[-1*j,2::7]*100)**2)**0.5/vector_scale)
 
 import matplotlib.pyplot as plt
-fig, ax = plt.subplots(figsize=(10, 10),facecolor='white')
+fig, ax = plt.subplots(figsize=(6, 8),facecolor='white')
 
 pcord = np.array([1000,975,950,925,900,875,850,825,800,775,750,700,
         650,600,550,500,450,400,350,300,250,225,200,175,150,125,100,70,
         50,30,20,10,7,5,3,2,1])
 
 ycord = np.arange(-90, 90.1, 1.25)
-# ylon=([ 50, 10, 5, 1])
-# chei=([ "50", "10", "5", "1"])
 ylon=([1000, 500, 100, 50, 10, 5, 1])
 chei=(["1000", "500", "100", "50", "10", "5", "1"])
 ax.set_ylim(lim,1.0e-1)
@@ -84,14 +76,17 @@ ax.set_xlim(30,80)
 ax.set_yscale('log')
 ax.set_yticks(ylon)
 ax.set_yticklabels(chei)
+ax.set_xlabel('LAT')
+ax.set_ylabel('pressure')
 
 for a in range(len(pcord)):
     if pcord[a] == lim:
         num = a
 
 X,Y=np.meshgrid(ycord,pcord)
+cont = plt.contourf(X,Y,nablaF)
 q = plt.quiver(X[num:,2::mabiki], Y[num:,2::mabiki], Fy[num:,2::mabiki], Fz[num:,2::mabiki]*100,pivot='middle',
                 scale_units='xy', headwidth=5,scale=vector_scale, color='green',width=0.005)
+plt.rcParams['image.cmap'] = 'bwr'
+plt.colorbar(cont)
 plt.savefig('./warota.png')
-# %%
-
